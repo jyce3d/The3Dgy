98b5746f3d37cd93a6fa680a64f7542234bc93f1
jyce3d <42608645+jyce3d@users.noreply.github.com>
2018-09-05 23:51:02 +0200

    Creation

---
+++ src/be/sdsg/apps3D/The3Dgy.java
@@ -0,0 +1,10 @@
+package be.sdsg.apps3D;
+
+import javax.swing.JFrame;
+
+public class The3Dgy extends JFrame {
+
+	public static void main(String args[]) {
+		
+	}
+}
---
+++ src/be/sdsg/script3D/ContextInt.java
@@ -0,0 +1,9 @@
+/* Interface sur le context */
+package be.sdsg.script3D;
+
+public interface ContextInt {
+	
+	public float getNumVar(String varName);
+	public String getTextVar(String varName);
+	
+}
